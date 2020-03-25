ea0048bcc0643bc1e52ace6f08255b26969f34ad
Willian Watanabe <watinha@gmail.com>
2020-03-25 15:52:20 -0300

    extracting the histogram of the roib and roit and saving it in the dataset file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, np, pandas as pd
+import sys, np, os, shutil, pandas as pd
 
 from PIL import Image
 from sklearn.cluster import KMeans
@@ -19,15 +19,26 @@ if len(sys.argv) < 5:
     print('forth argument should the size of the image to be considered...')
     sys.exit(1)
 
+if len(sys.argv) < 6:
+    print('fifth argument should be the directory to output the results...')
+    sys.exit(1)
+
 f1 = sys.argv[1]
 f2 = sys.argv[2]
 n_clusters = int(sys.argv[3])
 dim = int(sys.argv[4])
+folder = sys.argv[5]
 
 i1 = Image.open(f1)
 i2 = Image.open(f2)
-i1.save('output/screenshot-baseline.png')
-i2.save('output/screeenshot-test.png')
+
+if os.path.exists('./output/%s' % (folder)):
+    print('output directory exists... is it ok?')
+    sys.exit(1)
+
+os.mkdir('./output/%s' % (folder))
+i1.save('output/%s/screenshot-baseline.png' % (folder))
+i2.save('output/%s/screeenshot-test.png' % (folder))
 
 diff_width = i2.size[0] - i1.size[0]
 min_height = min(i1.size[1], i2.size[1])
@@ -35,8 +46,8 @@ crop_area = (int(diff_width / 2), 0, i1.size[0] + int(diff_width / 2), min_heigh
 i1 = i1.convert('L')
 i2 = i2.convert('L').crop(crop_area)
 #i2 = i2.convert('L').resize(i1.size)
-i1.save('output/screenshot-baseline-croped.png')
-i2.save('output/screeenshot-test-croped.png')
+i1.save('output/%s/screenshot-baseline-croped.png' % (folder))
+i2.save('output/%s/screenshot-test-croped.png' % (folder))
 
 diff = Image.new('RGBA', i1.size, (0, 0, 0, 0))
 clustered_image = Image.new('RGBA', i1.size, (0, 0, 0, 0))
@@ -64,8 +75,8 @@ for i in range(len(clusters)):
     if color_hash >= 510: color = (0, 0, color_hash - 510, d[2])
     clustered_image.putpixel((d[0], d[1]), color)
 
-diff.save('output/result.png')
-clustered_image.save('output/clustered-result.png')
+diff.save('output/%s/result.png' % (folder))
+clustered_image.save('output/%s/clustered-result.png' % (folder))
 
 # --- clustering data
 clustered_diffs = [ [] for i in range(n_clusters) ]
@@ -90,18 +101,24 @@ for cluster in clustered_diffs:
         for d in cluster:
             new_image.putpixel((d[0] - left, d[1] - top), (255, 0, 0, d[2]))
             new_image2.putpixel((d[0] - left, d[1] - top), (d[2]))
-        new_image.save('output/cluster-%d.png' % (count))
+        new_image.save('output/%s/cluster-%d.png' % (folder, count))
         new_image2 = new_image2.resize((dim, dim))
         row = new_image2.histogram()
         row.append(left)
         row.append(right)
         row.append(top)
         row.append(down)
-        row.append('output/cluster-%d.png' % (count))
+        row.append('output/%s/cluster-%d.png' % (folder, count))
         for i in range(dim):
             for j in range(dim):
                 row.append(new_image2.getpixel((i, j)))
-        dataset[clustered_diffs.index(cluster)] = row
+        roib = i1.crop((left, top, right, down))
+        roib_histogram = np.histogram(roib.histogram())[0].tolist()
+        roit = i2.crop((left, top, right, down))
+        roit_histogram = np.histogram(roit.histogram())[0].tolist()
+        roib.save('output/%s/cluster-%d-base.png' % (folder, count))
+        roit.save('output/%s/cluster-%d-target.png' % (folder, count))
+        dataset[clustered_diffs.index(cluster)] = row + roib_histogram + roit_histogram
         count += 1
 
 col = [ ('bin_%d' % (c)) for c in range(256) ]
@@ -111,8 +128,15 @@ col.append('top')
 col.append('down')
 col.append('diff_image')
 col2 = [ '%dx%d-pixel' % (i,j) for i in range(dim) for j in range(dim) ]
-frame = pd.DataFrame(dataset, columns=(col+col2))
-frame.to_csv('output/dataset.csv')
-frame.to_json('output/dataset.json', orient='split')
+col3 = [ ('roib_bin_%d' % (c)) for c in range(10) ] # 10 bins Browserbite
+col4 = [ ('roit_bin_%d' % (c)) for c in range(10) ] # 10 bins Browserbite
+frame = pd.DataFrame(dataset, columns=(col+col2+col3+col4))
+frame.to_csv('output/%s/dataset.csv' % (folder))
+json = frame.to_json(orient='split')
+f = open('output/%s/dataset.json' % (folder), 'w')
+f.write('let dataset = %s' % (json))
+f.close()
+
+shutil.copyfile('output/index.html', ('output/%s/index.html' % (folder)))
 
 sys.exit(0)
